964fa0d00b301e07408e7782cb0fd7a04de8546a
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-24 07:16:40 +0000

    Change module name to

---
+++ src/app/writing/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Blogs',
+  description: "Read technical articles and blog posts by Kofi Kusi covering web development, AI/ML, computer science, and reflections on university life.",
+
+}
+
+export default function Writing() {
+  return <h1>writing</h1>;
+}
