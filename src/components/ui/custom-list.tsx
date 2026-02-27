e473f9c54afc5816db2b9cfe5b7305cc90752af2
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-27 10:07:19 +0000

    Add animations to arrow icons on group hover

--- src/components/ui/custom-list.tsx
+++ src/components/ui/custom-list.tsx
@@ -17,13 +17,13 @@ export function CustomList({ children } : CustomListProps) {
 
 export function CustomListItem({ children, isSubItem }: CustomListItemProps) {
     return (
-        <li className={`flex items-center gap-2 ${isSubItem ? "ml-6" : ""}`}>
+        <li className={`flex justify-center w-fit gap-2 transition-all duration-300 hover:translate-x-2 group ${isSubItem ? "ml-6" : ""}`}>
           {
             isSubItem ? (
-              <ArrowBigRightDash size={16} className=""/>
+              <ArrowBigRightDash size={16} className="mt-1 flex-shrink-0 transition-all duration-300 group-hover:rotate-[20deg] text-foreground opacity-60"/>
             ) : (
 
-              <ArrowBigRight size={16} className=""/>
+              <ArrowBigRight size={16} className="mt-1 flex-shrink-0 transition-all duration-300 group-hover:rotate-[20deg] text-foreground opacity-70"/>
             )
           }
           <span>{children}</span>
