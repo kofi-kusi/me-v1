b066326159e8c67728aaec82abe4d5d7ced1bcf8
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-27 10:07:59 +0000

    Create status page

--- src/components/home/status.tsx
+++ src/components/home/status.tsx
@@ -1,11 +1,78 @@
 import { CustomListItem, CustomList } from "../ui/custom-list";
+import Link from "next/link";
+import { ArrowBigDownDash } from "lucide-react";
 export default function Status() {
-    return (
-        <div>
-            <CustomList>
-            <CustomListItem> here</CustomListItem>
-            <CustomListItem isSubItem>Hello</CustomListItem>
-            </CustomList>
-        </div>
-    )
+  return (
+    <div>
+      <section className="my-4" >
+        <CustomList>
+          <CustomListItem>
+            <p className="font-bold">Currently:</p>
+          </CustomListItem>
+          <CustomListItem isSubItem>
+            Studying CS @{" "}
+            <Link href="https://kstu.edu.gh" target="_blank">
+              Kumasi Technical University
+            </Link>
+          </CustomListItem>
+          <CustomListItem isSubItem>
+            Learning stuff on @{" "}
+            <Link href="https://oreilly.com">O&apos;Reilly</Link>
+          </CustomListItem>
+          <CustomListItem isSubItem>
+            Exploring ML with
+            <ArrowBigDownDash
+              size={16}
+              className="text-foreground opacity-60 ml-16"
+            />
+            <Link href="https://learning.oreilly.com/library/view/hands-on-machine-learning/9798341607972/">
+              Hands-On Machine Learning with Scikit-Learn and PyTorch
+            </Link>
+          </CustomListItem>
+        </CustomList>
+      </section>
+
+      <section className="my-4">
+        <CustomList>
+          <CustomListItem>
+            <p className="font-bold">Recently:</p>
+          </CustomListItem>
+          <CustomListItem isSubItem>
+            Software Developer intern @{" "}
+            <Link href="https://microlynxe.com/" target="_blank">
+              Microlynxe Systems Ltd
+            </Link>
+          </CustomListItem>
+          <CustomListItem isSubItem>
+            Club Lead  @{" "}
+            <Link href="https://csclub.uwaterloo.ca" target="_blank">
+              AmaliTech Coding Club KsTU
+            </Link>
+          </CustomListItem>
+        </CustomList>
+      </section>
+
+      
+      <section className="my-4">
+        <CustomList>
+          <CustomListItem>
+            I <Link href="/writing">write</Link>.
+          </CustomListItem>
+        </CustomList>
+      </section>
+
+      
+      <section className="my-4">
+        <CustomList>
+          <CustomListItem>
+            View my{" "}
+            <Link href="/resume.pdf" prefetch={false}>
+              resume
+            </Link>
+            .
+          </CustomListItem>
+        </CustomList>
+      </section>
+    </div>
+  );
 }
