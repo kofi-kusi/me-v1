16bcff26f328aedb55c87379e1ee6acd2f3596ab
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-27 10:43:46 +0000

    Create footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,20 @@
+import { Copyright } from "lucide-react";
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <hr />
-      <h1>Footer goes here</h1>
+      <div className="mt-2 flex flex-col justify-center items-center">
+        <p className=" flex gap-2 items-center">
+          {currentYear}
+          <span>
+            <Copyright size={16} className="" />
+          </span>
+          Kofi Kusi Appau.
+        </p>
+        <p className="text-foreground opacity-50">All Rights Reserved.</p>
+      </div>
     </footer>
   );
 }
