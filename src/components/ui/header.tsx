2d4c36198f7522bacf4ca4f2757d31c699f7e9d7
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-27 12:58:38 +0000

    Add social links

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,44 +1,66 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeToggle from "../ThemeToggle";
 import { FileUser } from "lucide-react";
+import SocialIcons from "../social-links";
 
 export default function Header() {
-    const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <header>
-      <div className="flex justify-between items-center">
-        <div>
-        <h1 className="font-black text-lg" >kofi kusi</h1>
-        <p><i>Software Engineer</i></p>
-        </div>
+      <div className="flex justify-between">
+        <h1 className="font-black text-lg text-primary">kofi kusi</h1>
         <div className="flex gap-4 items-center">
           <ThemeToggle />
-            <Link href="resume.pdf" className="hover:text-primary">
+          <Link href="resume.pdf" className="hover:text-primary">
             <div className="flex items-center gap-2 border-2 border-primary-hover rounded-sm px-2 py-[1.5px]">
-                <FileUser size={16} />
-                Resume
+              <FileUser size={16} />
+              Resume
             </div>
-            </Link>
-          
-          </div>
+          </Link>
+        </div>
       </div>
-      <hr className="my-2"/>
-      <nav >
+
+      <div className="flex justify-between items-center my-4">
+        <p>
+          <i>Software Engineer</i>
+        </p>
+        <SocialIcons />
+      </div>
+
+      <hr className="my-2" />
+      <nav>
         <ul className="flex gap-6">
           <li>
-            <Link href="/" className={`${pathname === "/" ? "active": ""}`}>Home</Link>
+            <Link href="/" className={`${pathname === "/" ? "active" : ""}`}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/about" className={`${pathname === "/about" ? "active": ""}`}>About</Link>
+            <Link
+              href="/about"
+              className={`${pathname === "/about" ? "active" : ""}`}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link href="/works" className={`${pathname === "/works" ? "active": ""}`}>Works</Link>
+            <Link
+              href="/works"
+              className={`${pathname === "/works" ? "active" : ""}`}
+            >
+              Works
+            </Link>
           </li>
           <li>
-            <Link href="/writing" className={`${pathname === "/writing" ? "active": ""}`}>Writing</Link>
+            <Link
+              href="/writing"
+              className={`${pathname === "/writing" ? "active" : ""}`}
+            >
+              Writing
+            </Link>
           </li>
         </ul>
       </nav>
