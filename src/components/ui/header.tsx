e994f7f4c43158a1769fe9f177539e22ad8eb699
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-02-27 11:55:57 +0000

    Style resume button

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,16 +3,27 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeToggle from "../ThemeToggle";
+import { FileUser } from "lucide-react";
 
 export default function Header() {
     const pathname = usePathname()
   return (
     <header>
       <div className="flex justify-between items-center">
-        <div className="font-black " >kofi kusi</div>
-        <div className="flex gap-2 items-center">
+        <div>
+        <h1 className="font-black text-lg" >kofi kusi</h1>
+        <p><i>Software Engineer</i></p>
+        </div>
+        <div className="flex gap-4 items-center">
           <ThemeToggle />
-          Resume</div>
+            <Link href="resume.pdf" className="hover:text-primary">
+            <div className="flex items-center gap-2 border-2 border-primary-hover rounded-sm px-2 py-[1.5px]">
+                <FileUser size={16} />
+                Resume
+            </div>
+            </Link>
+          
+          </div>
       </div>
       <hr className="my-2"/>
       <nav >
