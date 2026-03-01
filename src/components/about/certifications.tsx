fd9ab0609e04f6692adf67c50481a063a4156410
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-03-01 15:17:43 +0000

    Fix badge image url

--- src/components/about/certifications.tsx
+++ src/components/about/certifications.tsx
@@ -7,14 +7,14 @@ export default function Certs() {
       <ul className="ml-6 my-4">
         <Badge
           url="https://www.credly.com/badges/ce735840-2053-4b7e-95b9-2abaf73b0e77/public_url"
-          imgUrl="/fastapi-backend-dev.png"
+          imgUrl="/badges/fastapi-backend-dev.png"
           name="FastAPI & Backend Dev"
           from="O'Reilly"
         />
 
         <Badge
           url="https://www.credly.com/badges/740cc9f3-2f79-457c-a372-ca501814440b/public_url"
-          imgUrl="/data-structures-algorithms.4.png"
+          imgUrl="/badges/data-structures-algorithms.4.png"
           name="Data structures and algorithms"
           from="O'Reilly"
         />
