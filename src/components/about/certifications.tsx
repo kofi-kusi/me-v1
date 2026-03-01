9a8710168acee2c6f71ef19295ff7dc377eed272
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-03-01 15:12:38 +0000

    Add certifications section

---
+++ src/components/about/certifications.tsx
@@ -0,0 +1,24 @@
+import Badge from "@/components/ui/badge";
+
+export default function Certs() {
+  return (
+    <section>
+      <h2>Certifications:</h2>
+      <ul className="ml-6 my-4">
+        <Badge
+          url="https://www.credly.com/badges/ce735840-2053-4b7e-95b9-2abaf73b0e77/public_url"
+          imgUrl="/fastapi-backend-dev.png"
+          name="FastAPI & Backend Dev"
+          from="O'Reilly"
+        />
+
+        <Badge
+          url="https://www.credly.com/badges/740cc9f3-2f79-457c-a372-ca501814440b/public_url"
+          imgUrl="/data-structures-algorithms.4.png"
+          name="Data structures and algorithms"
+          from="O'Reilly"
+        />
+      </ul>
+    </section>
+  );
+}
--- src/components/about/me.tsx
+++ src/components/about/me.tsx
@@ -1,9 +1,9 @@
 export default function Me() {
   return (
     <section className="transition-all duration-300 hover:translate-x-2">
-      <h1>Who am i?:</h1>
+      <h2>Who am i?</h2>
       <div className="mt-2">
-        <p>
+        <p className="ml-6">
           I’m a Computer Science student building full-stack web applications with <b>React</b>, <b>Next.js</b>, and <b>FastAPI</b>. I’m currently diving deep into <b>machine learning</b>, with a growing interest in how intelligent systems are designed and deployed. I enjoy breaking down complex problems and turning them into structured, well-engineered solutions.
         </p>
       </div>
