8c4ed302251c2148a44437fa7fc684a62b921d93
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-03-01 15:14:45 +0000

    Add some certifications

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Me from "@/components/about/me"
+import Certs from "@/components/about/certifications";
 
 export const metadata: Metadata = {
   title: 'About',
@@ -11,6 +12,7 @@ export default function About() {
   return (
     <div className="my-4 flex flex-col gap-4">
       <Me />
+      <Certs />
     </div>
   );
 }
