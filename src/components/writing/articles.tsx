26f8f00622e5fe015aa460b523c61bd3dba1e253
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-03-04 09:56:25 +0000

    Display article cards

--- src/components/writing/articles.tsx
+++ src/components/writing/articles.tsx
@@ -1,13 +1,22 @@
 import articlesData from "@/data/writing.json"
+import ArticleCard from "./article-card"
 
 export default function Articles(){
     return (
-        <>
+        <div className="flex flex-col gap-2">
         {
             articlesData.map(article => {
-                return <h1 key={article.filename}>{article.title}</h1>
+                return (
+                    <ArticleCard
+                        key={article.filename}
+                        title={article.title}
+                        date={article.date}
+                        slug={article.slug}
+                        subject={article.subject}
+                    />
+                )
             })
         }
-        </>
+        </div>
     )
 }
